4eeac831ef491437dae3af2069ea7c81f388297d
PotOfCoffee2Go <PotOfCoffee2Go@users.noreply.github.com>
2025-07-24 06:03:52 -0400

    refactor twgist.js and twrepo.js

--- twgist.js
+++ twgist.js
@@ -4,73 +4,16 @@ const commander = require('commander');
 const program = new commander.Command();
 
 const pkg = require('./package.json');
-const {twStatus} = require('./lib/status');
-const {twInit} = require('./lib/init');
-const {twPull} = require('./lib/pull');
-const {twPush} = require('./lib/push');
 const {twConfig} = require('./lib/config');
-
-const settings = twConfig();
-
-function myParseInt(value) {
-	// parseInt takes a string and a radix
-	const parsedValue = parseInt(value, 10);
-	if (isNaN(parsedValue)) {
-		throw new commander.InvalidArgumentError('Not a number.');
-	}
-	return parsedValue;
-}
+const {prArgs} = require('./lib/args');
 
 program
 	.name('twgist')
-	.description('CLI to store TiddlyWikis to GitHub Gists')
+	.description('CLI to store TiddlyWikis to GitHub Repos')
 	.version(pkg.version);
 
-program
-	.command('status')
-	.description('Git status of tiddlers in wiki')
-	.argument('[wiki]', 'tiddywiki server editon wiki directory', '.')
-	.option('-s, --server', 'start server after command')
-	.option('-p, --port <port>', 'server port', myParseInt, 8080)
-	.option('-l, --log', 'display recent commits')
-	.action((dir, options) => {
-		options.settings = settings;
-		twStatus(dir, options);
-	});
+const settings = twConfig(program.name());
 
-program
-	.command('init')
-	.description('Create a TiddlyWiki from a GitHub Gist.')
-	.argument('<wiki>', 'directory to init a server edition wiki')
-	.requiredOption('-g, --gistid  <gistid>', 'gist id that contains tiddlers')
-	.action((dir, options) => {
-		options.settings = settings;
-		twInit(dir, options);
-	});
-
-program
-	.command('pull')
-	.description('Pull wiki tiddlers from GitHub Gist')
-	.argument('[wiki]', 'tiddywiki server editon wiki directory', '.')
-	.option('-s, --server', 'start server after command')
-	.option('-p, --port <port>', 'server port', myParseInt, 8080)
-	.option('-l, --log', 'display recent commits')
-	.action((dir, options) => {
-		options.settings = settings;
-		twPull(dir, options);
-	});
-
-program
-	.command('push')
-	.description('Push wiki tiddlers to GitHub Gist')
-	.argument('[wiki]', 'tiddywiki server editon wiki directory', '.')
-	.option('-m, --message <string>', 'commit message', 'Wiki Updates')
-	.option('-s, --server', 'start server after command')
-	.option('-p, --port <port>', 'server port', myParseInt, 8080)
-	.option('-l, --log', 'display recent commits')
-	.action((dir, options) => {
-		options.settings = settings;
-		twPush(dir, options);
-	});
+prArgs(commander, program, settings);
 
 program.parse();
